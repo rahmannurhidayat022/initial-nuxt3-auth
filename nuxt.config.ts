cf81e13996315fe7555dfb923237ba1ddd680f28
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2024-02-03 20:32:59 +0700

    feat: nuxt conf and environtment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: { enabled: false },
+	runtimeConfig: {
+		public: {
+			baseUrl: process.env.BASE_URL,
+			cookieName: "__session_",
+			cookieOptions: {
+				maxAge: 172800,
+				sameSite: "lax",
+			},
+		},
+	},
+	app: {
+		buildAssetsDir: "__app_name_",
+	},
+	css: ["~/assets/css/main.css"],
+	nitro: {
+		routeRules: {
+			"/app/**": {
+				proxy: {
+					to: process.env.API_HOST + "/**",
+				},
+			},
+		},
+	},
+});
